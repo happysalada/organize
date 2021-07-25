794927ae3862e0cc1ba3a431a442640958df36a4
happysalada <raphael@megzari.com>
2021-07-25 21:04:07 +0900

    purge css: add dynamic classes to safelist

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,6 +14,21 @@ module.exports = {
       ],
       keyframes: true,
     },
+    // dynamic classes
+    safelist: [
+      "bg-blue-500",
+      "bg-red-500",
+      "bg-green-500",
+      "bg-purple-500",
+      "bg-blue-100",
+      "bg-red-100",
+      "bg-green-100",
+      "bg-purple-100",
+      "text-blue-800",
+      "text-red-800",
+      "text-green-800",
+      "text-purple-800",
+    ],
   },
   theme: {
     extend: {},
