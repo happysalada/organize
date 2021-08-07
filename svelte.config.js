dd577babd0f00a79e2b4b1b8d2f7458782dd88a4
happysalada <raphael@megzari.com>
2021-08-07 16:24:55 +0900

    remove markdown parser

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,12 @@
-import { mdsvex } from "mdsvex";
-import mdsvexConfig from "./mdsvex.config.cjs";
 import preprocess from "svelte-preprocess";
 import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  extensions: [".svelte", ...mdsvexConfig.extensions],
+  extensions: [".svelte"],
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
   preprocess: [
-    mdsvex(mdsvexConfig),
     preprocess({
       defaults: {
         style: "postcss",
